3d0ed44c222a87c6e6c899b1456e62ef8f80a9aa
KotonoSora <26807417+KotonoSora@users.noreply.github.com>
2026-06-30 10:55:07 +0700

    feat: 🎸 remove config theme

--- app/src/main/java/com/jn/trixo/ui/theme/Theme.kt
+++ app/src/main/java/com/jn/trixo/ui/theme/Theme.kt
@@ -5,7 +5,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
@@ -49,8 +48,6 @@ fun TrixoTheme(
             val context = view.context
             if (context is Activity) {
                 val window = context.window
-                window.statusBarColor = colorScheme.background.toArgb()
-                window.navigationBarColor = colorScheme.background.toArgb()
                 WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = false
                 WindowCompat.getInsetsController(window, view).isAppearanceLightNavigationBars =
                     false
